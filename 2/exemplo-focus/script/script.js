32c7604e6228c9ab388bacdfcf4fb0bd35b9e1ee
Vanessa Ariel <vanessa.ariel24@gmail.com>
2018-09-19 14:01:38 -0300

    Adjustments to focus exercise

--- 2/exemplo-focus/script/script.js
+++ 2/exemplo-focus/script/script.js
@@ -1,18 +1,21 @@
 //DECLARE ALL INPUTS
 const inputName = document.getElementById("cadastroInputNome");
 const inputEmail = document.getElementById("cadastroInputEmail");
-const inputEmailConfirm = document.getElementById("cadastroInputEmail");
+const inputEmailConfirm = document.getElementById("cadastroInputEmailConfirm");
 const inputPassword = document.getElementById("cadastroInputPassword");
-const inputPasswordConfirm = document.getElementById("cadastroInputPassword");
+const inputPasswordConfirm = document.getElementById("cadastroInputPasswordConfirm");
 const inputPhone = document.getElementById("cadastroInputPhone");
 const inputArea = document.getElementById("cadastroInputArea");
 const checkBox = document.getElementById("cadastroInputNews");
 
 const inputLevel = document.getElementsByName("level");
 
+//ADD BUTTON
 const button = document.querySelector(".cadastro__button");
 button.addEventListener("click",function(event){
-    event.preventDefault();
+    event.preventDefault(); 
+        
+        //ADD CONDITIONS TO CHECK IF FORMS ARE VALID 
         if (inputName.value === undefined || inputName.value === null || inputName.value ==="" || inputName.value ===" "){
             inputName.focus();
             return false;
@@ -25,7 +28,8 @@ button.addEventListener("click",function(event){
             inputEmailConfirm.focus();
             return false;
 
-        }else if (inputEmailConfirm.value !== inputEmailConfirm.value){
+        //CHECK IF EMAILS MATCH
+        }else if (inputEmail.value !== inputEmailConfirm.value){ 
             alert ("The emails do not match!");
             
         }else if (inputPassword.value === undefined || inputPassword.value === null || inputPassword.value ==="" || inputPassword.value ===" "){
@@ -36,6 +40,12 @@ button.addEventListener("click",function(event){
             inputPasswordConfirm.focus();
             return false;
 
+        //CHECK IF PASSWORD HAS AT LEAST 7 CHARACTERS
+        }else if (inputPassword.value.length <= 7){
+            inputPassword.focus();
+            return false
+
+        //CHECK IF PASSWORDS MATCH
         }else if (inputPassword.value !== inputPasswordConfirm.value){
             alert ("The passwords do not match!");
         
@@ -43,10 +53,24 @@ button.addEventListener("click",function(event){
             inputPhone.focus();
             return false;
     
+        //VERIFY IF CHECKBOX IS CHECKED
         }else if (checkBox.value !== checkBox.checked){
             checkBox.focus();
             return false;
         }
+})
+
+//METHOD TO CHANGE BACKGROUND COLOR
+inputArea.addEventListener("change",function(){
+    if (inputArea.selectedIndex === 0){
+        document.querySelector("body").style.backgroundColor = "#e3f2fd"
+    } else if (inputArea.selectedIndex ===1){
+        document.querySelector("body").style.backgroundColor = "#e3f2fd" //CHANGE COLOR
+    } else if (inputArea.selectedIndex ===2){
+        document.querySelector("body").style.backgroundColor = "#e3f2fd" //CHANGE COLOR
+    } else if (inputArea.selectedIndex ===3){
+        document.querySelector("body").style.backgroundColor = "#e3f2fd" //CHANGE COLOR
+    }
 })
 
 button.addEventListener("click",function(e){
@@ -67,4 +91,8 @@ button.addEventListener("click",function(e){
     }else if (radioItem.value === 2){
         console.log("5+ anos de experiência");
     }
+
+    document.querySelector("body").style.backgroundColor = "#fff";
+    const form = this.closest("form");
+    form.submit();
 })
