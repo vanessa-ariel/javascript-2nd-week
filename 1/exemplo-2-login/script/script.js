a10269898f2f1b7b8c91f1de415c8d690fe89dcd
Vanessa Ariel <vanessa.ariel24@gmail.com>
2018-09-25 21:47:46 -0300

    Add innerHTML

--- 1/exemplo-2-login/script/script.js
+++ 1/exemplo-2-login/script/script.js
@@ -1,23 +1,19 @@
-//const loginButton = document.querySelector(".login__button");
-//const input = document.getElementById("loginInputEmail");
- //.login__button is a string
+//ADD BUTTON
+const loginButton = document.querySelector(".login__button");
 
- //loginButton.addEventListener("click", function(event){ //parameters do not receive quotation marks
-    //event.preventDefault();
-    //const email = input.value;
-    //const loginForm = document.querySelector(".login__form");
-    //loginForm.innerHTML = `You have successfully logged in as ${email}!`
-    //alert(`You have successfully logged in as ${email}!`);
-//})
+//ADD INPUT
+const input = document.getElementById("loginInputEmail");
 
-
-
-/* ITERATIONS
-
-Inform that I am using a HTML element with document.querySelector
-
-Create a variable for this element and assign a name and a string for the query selector that contains a class ex. (".class_name")
-
-Sign that something needs to be done on login Button when user clicks on it using the method .addEventListener. The first argument is "click" by default, the second one is a function
-
-*/
+//ADD EVENT LISTENER TO BUTTON
+loginButton.addEventListener("click", function(event){
+    event.preventDefault();
+	
+	//DECLARE EMAIL
+    const email = input.value;
+	
+	//ADD LOGIN FORM
+	const loginForm = document.querySelector(".login__form");
+	
+	//INSERT TEXT TO LOGIN FORM ON HTML
+    loginForm.innerHTML = `You have successfully logged in as ${email}!!`;
+})
